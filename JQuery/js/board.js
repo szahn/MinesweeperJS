0b042e445eee07715b234d5ad309647e6e85fd24
Stuart Zahn <stuart.zahn@gmail.com>
2015-09-09 22:03:14 -0700

    refactored map

--- JQuery/js/board.js
+++ JQuery/js/board.js
@@ -13,14 +13,26 @@ var Board = function(width, height, mineCount){
 	this.mines = []
 };
 
-Board.prototype = {
+Board.prototype = {	
+
+	getCell: function(x, y){
+		return this.map[this.coordToCellIndex(x, y)];
+	},
+
+	setCell: function(x, y, flags){
+		this.map[this.coordToCellIndex(x, y)] = flags;
+	},
+
+	coordToCellIndex: function(x, y){
+		return x + (y * (this.width+1));
+	},
+
 	clear: function(){
 		this.mines = []
 		var map = this.map;
 		for (var x = 0;x < this.width;x ++){
-			map[x] = [];				
 			for (var y = 0;y <  this.height;y ++){
-				map[x][y] = CELL_EMPTY;				
+				this.setCell(x, y, CELL_EMPTY);				
 			}
 		}
 	},
@@ -28,7 +40,7 @@ Board.prototype = {
 	haveAllMinesBeenFlagged: function(){
 		for (var i = 0; i < this.mines.lengnth; i++){
 			var mine = this.mines[i];
-			if ((this.map[mine[0]][mine[1]] & CELL_FLAG) != CELL_FLAG){
+			if ((this.getCell(mine[0], mine[1]) & CELL_FLAG) != CELL_FLAG){
 				return false;
 			}
 		}
@@ -40,26 +52,26 @@ Board.prototype = {
 		var map = this.map;
 		for (var x = 0;x < this.width;x ++){
 			for (var y = 0;y <  this.height;y ++){
-				if ((map[x][y] & CELL_MINE) == CELL_MINE)
-					map[x][y] = map[x][y] | CELL_VISIBLE;				
+				var flags = this.getCell(x,y);
+				if ((flags & CELL_MINE) == CELL_MINE)
+					this.setCell(x, y, flags | CELL_VISIBLE);				
 			}
 		}
 	},
 
 	floodFill: function(x, y, checked){
-		if ((this.map[x][y] & CELL_MINE) == CELL_MINE)
-			return;
+		var flags = this.getCell(x, y);
+		if ((flags & CELL_MINE) == CELL_MINE) return;
+		if (checked[x] && checked[x][y] == true) return;
 
-		if (checked[x] && checked[x][y] == true)
-			return;
+		this.setCell(x, y, flags | CELL_VISIBLE);
 
-		this.map[x][y] = this.map[x][y] | CELL_VISIBLE;
 		if (!checked[x]) checked[x]  = [];
 		checked[x][y] = true;
 
 		for (var x1 = Math.max(0, x - 1); x1 < Math.min(x + 1, this.width); x1++){
 			for (var y1 = Math.max(0, y - 1); y1 < Math.min(y + 1, this.width); y1++){
-				if ((this.map[x1][y1] & CELL_MINE) == CELL_MINE) return;
+				if ((this.getCell(x1, y1) & CELL_MINE) == CELL_MINE) return;
 			}
 		}
 
@@ -70,16 +82,18 @@ Board.prototype = {
 	},
 
 	hitCell: function(x, y){
-		if ((this.map[x][y] & CELL_MINE) == CELL_MINE){
+		var flags = this.getCell(x, y);
+
+		if ((flags & CELL_MINE) == CELL_MINE){
 			this.revealMines();
 			return true;
 		}
 
-		if ((this.map[x][y] & CELL_FLAG) == CELL_FLAG){
-			this.map[x][y] = this.map[x][y] ^ CELL_FLAG;
+		if ((flags & CELL_FLAG) == CELL_FLAG){
+			this.setCell(x, y, flags ^ CELL_FLAG);
 		}
 
-		if ((this.map[x][y] & CELL_EMPTY) == CELL_EMPTY) {
+		if ((flags & CELL_EMPTY) == CELL_EMPTY) {
 			this.floodFill(x, y, []);			
 		}
 
@@ -90,8 +104,8 @@ Board.prototype = {
 		var count = 0;
 		for (var x = 0;x < this.width;x ++){
 			for (var y = 0;y <  this.height;y ++){
-				if ((this.map[x][y] & CELL_MINE) == CELL_MINE && 
-					!((this.map[x][y] & CELL_FLAG) == CELL_FLAG)){
+				var flags = this.getCell(x, y);
+				if ((flags & CELL_MINE) == CELL_MINE && !((flags & CELL_FLAG) == CELL_FLAG)){
 					count +=1;
 				}				
 			}
@@ -101,15 +115,17 @@ Board.prototype = {
 	},
 
 	flagCell: function(x, y){
-		if ((this.map[x][y] & CELL_VISIBLE) == CELL_VISIBLE)
+		var flags = this.getCell(x, y);
+
+		if ((flags & CELL_VISIBLE) == CELL_VISIBLE)
 			return 0;
 
-		if ((this.map[x][y] & CELL_FLAG) == CELL_FLAG){
-			this.map[x][y] = this.map[x][y] ^ CELL_FLAG;
+		if ((flags & CELL_FLAG) == CELL_FLAG){
+			this.setCell(x, y, flags ^ CELL_FLAG)
 			return 1;
 		}
 		else{
-			this.map[x][y] = this.map[x][y] | CELL_FLAG;
+			this.setCell(x, y, flags | CELL_FLAG)
 			return -1;
 		}
 	},
@@ -123,25 +139,26 @@ Board.prototype = {
 			var existingRow = existingRowEl.length == 1 ? existingRowEl : null;
 			var row = existingRow || $("<div id=\"" + rowId + "\" class=\"row\">");
 			for (var x = 0;x <  this.width;x ++){
+				var cellFlags = this.getCell(x, y);
 				var cellId = "cell" + x + "x" + y;
 				var existingCellEl = $("#" + cellId);
 				var existingCell = existingCellEl.length == 1 ? existingCellEl : null;
 				var cell = existingCell || $("<div id=\"" + cellId + "\" class=\"cell\">").attr({x: x, y: y});				
-				var isVisible = (map[x][y] & CELL_VISIBLE) == CELL_VISIBLE; 
-				var isFlagged = (map[x][y] & CELL_FLAG) == CELL_FLAG; 
+				var isVisible = (cellFlags & CELL_VISIBLE) == CELL_VISIBLE; 
+				var isFlagged = (cellFlags & CELL_FLAG) == CELL_FLAG; 
 				if (isVisible && !isFlagged){
-					if ((map[x][y] & CELL_MINE) == CELL_MINE)
+					if ((cellFlags & CELL_MINE) == CELL_MINE)
 					{
 						cell.removeClass("flag empty fa fa-flag").addClass("mine fa fa-bomb");
 					}				
-					if ((map[x][y] & CELL_EMPTY) == CELL_EMPTY)
+					if ((cellFlags & CELL_EMPTY) == CELL_EMPTY)
 					{
 						cell.removeClass("mine flag fa-bomb fa-flag fa").addClass("empty");
 						
 						var neighbors = 0;
 						for (var x1 = Math.max(0, x - 1); x1 <= Math.min(x + 1, this.width - 1); x1++){
 							for (var y1 = Math.max(0, y - 1); y1 <= Math.min(y + 1, this.height - 1); y1++){
-								if ((map[x1][y1] & CELL_MINE) == CELL_MINE) neighbors += 1;
+								if ((this.getCell(x1, y1) & CELL_MINE) == CELL_MINE) neighbors += 1;
 							}
 						}
 
@@ -170,7 +187,7 @@ Board.prototype = {
 	},
 
 	build: function(){
-		this.buildCluster(0, 4, 2);
+		this.buildCluster(0, 6, 3);
 		if (this.mines.length < this.numberOfMines){
 			this.build();
 		}
@@ -180,7 +197,7 @@ Board.prototype = {
 		var points = [];
 		for (var x = minX; x <= maxX; x++){
 			for (var y = minY; y <= maxY; y++){
-				if (this.map[x][y] === flag){
+				if (this.getCell(x, y) === flag){
 					points.push([x, y]);
 				}
 			}
@@ -199,7 +216,7 @@ Board.prototype = {
 		var points = [];
 		for (var x = Math.max(minX, lastPoint[0] - oneOrZero()); x <= Math.min(maxX, lastPoint[0] + oneOrZero()); x++){
 			for (var y = Math.max(minY, lastPoint[1] - oneOrZero()); y <= Math.min(maxY, lastPoint[1] + oneOrZero()); y++){
-				if (x != lastPoint[0] && y != lastPoint[1] && this.map[x][y] === CELL_EMPTY){
+				if (x != lastPoint[0] && y != lastPoint[1] && this.getCell(x, y) === CELL_EMPTY){
 					points.push([x, y]);
 				}
 			}
@@ -216,21 +233,25 @@ Board.prototype = {
 
 		var pointIndex = Math.floor(Math.random() * (points.length - 1));
 		var nextPoint = points[pointIndex];
-		this.map[nextPoint[0]][nextPoint[1]] = CELL_MINE;
-		this.mines.push(nextPoint);
+		this.placeMine(nextPoint[0], nextPoint[1]);
 		this.traverseCluster(nextPoint, minX, minY, maxX, maxY, minesRemaining - 1);
 	},
 
 	buildCluster: function(border, clusterSize, minesPerCluster){
-		var map = this.map;
 		var clusterSizeHalf = Math.round(clusterSize / 2);
 		var maxX = this.width - (1 + border);
 		var maxY = this.height - (1 + border);
 		var origin = this.getRandomCells(border, border, maxX, maxY, CELL_EMPTY);
-		map[origin[0]][origin[1]] = CELL_MINE;	
-		this.mines.push(origin);
+		this.placeMine(origin[0], origin[1]);
 		this.traverseCluster(origin, Math.max(border, origin[0] - clusterSizeHalf), Math.max(border, origin[1] - clusterSizeHalf),
 			Math.min(maxX, origin[0] + clusterSizeHalf), Math.min(maxY, origin[1] + clusterSizeHalf), minesPerCluster - 1);
+	},
+
+	placeMine: function(x, y){
+		if (this.getCell(x, y) !== CELL_EMPTY) return;
+
+		this.setCell(x, y, CELL_MINE);	
+		this.mines.push([x, y]);
 	}
 
 }
